66673563a7e11f27e1aeb36116828875168e0a20
durgesh656 <durgeshkushwaha656@gmail.com>
2025-03-04 19:55:56 +0530

    [Durgesh Kushwaha] Refactor: refactor the code to write a function to get work hours ✨

--- employee_wages/EmployeeWages.js
+++ employee_wages/EmployeeWages.js
@@ -16,18 +16,39 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
+// let empHrs = 0;
+// empCheck = Math.floor(Math.random() * 10) % 3;
 
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default : empHrs = 0;
+// switch(empCheck){
+//     case IS_PART_TIME:
+//         empHrs = PART_TIME_HOURS;
+//         break;
+//     case IS_FULL_TIME:
+//         empHrs = FULL_TIME_HOURS;
+//         break;
+//     default : empHrs = 0;
+// }
+
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("Employee Wage: " + empWage);
+
+// UC3 to get working hours
+function getWorkingHours(empCheck) {
+    switch(empCheck){
+        case IS_PART_TIME:
+            empHrs = PART_TIME_HOURS;
+            return empHrs;
+        case IS_FULL_TIME: 
+            empHrs = FULL_TIME_HOURS;
+            return empHrs;
+        default : empHrs = 0;
+            return empHrs;
+    }
 }
 
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+console.log("total working hours are: ",empHrs);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage: " + empWage);
