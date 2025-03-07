c3a0962b904101367b4682f2e56df145ab1acd11
durgesh656 <durgeshkushwaha656@gmail.com>
2025-03-08 02:21:22 +0530

    [Durgesh Kushwaha] Add: Ability to extend employee payroll data to store gender and start date added✨

--- employee_wages/EmployeePayroll.js
+++ employee_wages/EmployeePayroll.js
@@ -1,15 +1,17 @@
 class EmployeePayroll {
-    constructor(id, name, salary) {
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
     }
 
-    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}`;
+    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate}`;
 }
 
-const emp1 = new EmployeePayroll(101, "Durgesh", 35000);
-const emp2 = new EmployeePayroll(102, "John", 9000);
+const emp1 = new EmployeePayroll(101, "Durgesh", 35000, "Male", "2025-01-06");
+const emp2 = new EmployeePayroll(102, "Janny", 9000, "FeMale", "2025-01-06");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
